48226ed871cbcfa0235d65ff037d34b9be486a00
Alexis Tenailleau <alex-is-du85@hotmail.fr>
2026-02-21 18:44:32 +0100

    test

--- autoremovetorrents/compatibility/disk_usage_.py
+++ autoremovetorrents/compatibility/disk_usage_.py
@@ -1,21 +1,45 @@
+import os
 import sys
+import logging
+
+logger = logging.getLogger(__name__)
 
 # The shutil.disk_usage is available since Python 3.3,
 # but in other versions, we need to use psutil.disk_usage to replace it.
 # (For Synology's compatibility; there is no psutil in Python 3 in Synology)
 SUPPORT_SHUTIL = sys.version_info >= (3, 3, 0)
 
+def _resolve_path(path):
+    """Resolve the path to an absolute, real path and verify it exists."""
+    resolved = os.path.realpath(os.path.expanduser(path))
+    if not os.path.exists(resolved):
+        raise OSError("Path does not exist: %s (resolved to: %s)" % (path, resolved))
+    return resolved
+
 def disk_usage_(path):
+    resolved_path = _resolve_path(path)
+
+    if resolved_path != path:
+        logger.debug("Resolved path '%s' to '%s'" % (path, resolved_path))
+
     du = None
 
     if SUPPORT_SHUTIL:
         import shutil
-        du = shutil.disk_usage(path)
+        du = shutil.disk_usage(resolved_path)
     else:
         import psutil
-        du = psutil.disk_usage(path)
+        du = psutil.disk_usage(resolved_path)
 
-    # Unified format
+    # On macOS with APFS, statvfs may underreport free space because it
+    # excludes "purgeable" space. Log a warning so users know to check.
+    if sys.platform == 'darwin' and du.free == 0:
+        logger.warning(
+            "Reported free space is 0 for '%s'. "
+            "If this is an APFS volume, macOS may not be reporting purgeable space. "
+            "Verify with: diskutil info '%s'" % (resolved_path, resolved_path)
+        )
+
     return {
         'total': du.total,
         'used': du.used,
--- autoremovetorrents/condition/freespace.py
+++ autoremovetorrents/condition/freespace.py
@@ -1,13 +1,26 @@
 # -*- coding:utf-8 -*-
 
+from .. import logger
 from .freespacebase import FreeSpaceConditionBase
 from autoremovetorrents.compatibility.disk_usage_ import disk_usage_
+from ..util.convertbytes import convert_bytes
 
 class FreeSpaceCondition(FreeSpaceConditionBase):
     def __init__(self, settings):
         FreeSpaceConditionBase.__init__(self, settings)
         self._path = settings['path']
+        self._logger = logger.Logger.register(__name__)
 
     def apply(self, client_status, torrents):
-        free_space = disk_usage_(self._path)['free']
+        du = disk_usage_(self._path)
+        free_space = du['free']
+        self._logger.info(
+            'Local disk usage for %s: total=%s, used=%s, free=%s (min required: %s)' % (
+                self._path,
+                convert_bytes(du['total']),
+                convert_bytes(du['used']),
+                convert_bytes(free_space),
+                convert_bytes(self._min),
+            )
+        )
         FreeSpaceConditionBase.apply(self, free_space, torrents)
